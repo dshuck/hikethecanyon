1f8682bb890320794e0f3629a4b07621d5869ea5
Dave Shuck <dshuck@gmail.com>
2017-08-30 22:30:00 -0500

    Initial commit of bare-bones Spring Boot + Angular

---
+++ src/main/groovy/org/hikethecanyon/HikethecanyonApplication.groovy
@@ -0,0 +1,12 @@
+package org.hikethecanyon
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HikethecanyonApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run HikethecanyonApplication, args
+	}
+}
